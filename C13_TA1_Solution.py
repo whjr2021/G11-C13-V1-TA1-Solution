9e277192545e1b8499f4a112c1499e4fc75c4d68
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-23 10:48:05 +0530

    Add files via upload

---
+++ C13_TA1_Solution.py
@@ -0,0 +1,9 @@
+# Define a function with 2 inputs - first_name, second_name. Name it as "full_name". 
+# Function must concatenate the two inputs seperated by a space.
+
+def full_name (first_name, second_name):
+  name = first_name + ' ' + second_name
+  return name
+  
+# Call the "full_name" function by passing inputs and print the result function returns 
+print(full_name("Roger", "Federer")) 
